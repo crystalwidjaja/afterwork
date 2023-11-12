881e1d30e887149c0670f6777454655647f2d724
crystalwidjaja <pansypig@gmail.com>
2023-11-12 13:22:54 -0800

    Added logo page and put nav bar just on home page

--- src/app/nav.tsx
+++ src/app/nav.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Link from "next/link";
+import Logo from "./logo";
+
+// import Image from "next/AW_logo.PNG"
 
 const Navbar = () => {
   return (
     <>
-      <div className="w-full h-20 bg-emerald-800 sticky top-0">
+      <div className="w-full h-20 bg-white sticky top-0">
         <div className="container mx-auto px-4 h-full">
           <div className="flex justify-between items-center h-full">
-            <ul className="hidden md:flex gap-x-6 text-white">
+          <Logo />
+            <ul className="hidden md:flex gap-x-6 text-black">
               <li>
                 <Link href="/about">
                   <p>Case Studies</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image'
+import Navigation from "./nav";
 
 export default function Home() {
   return (
     <main>
+      <Navigation />
     </main>
   )
 }

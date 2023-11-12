b4959900086274681f701b4ee654d508a2324c65
crystalwidjaja <pansypig@gmail.com>
2023-11-12 13:34:56 -0800

    logo fix

--- src/app/logo.tsx
+++ src/app/logo.tsx
@@ -36,10 +36,10 @@ const Logo = () => {
     <>
       <Link href="/" style={{ display: showButton ? "none" : "block" }}>
         <Image
-          src="/public/AW_logo.png"
+          src="/AW_logo.png"
           alt="Logo"
-          width={width < 1024 ? "150" : "250"}
-          height={width < 1024 ? "45" : "74"}
+          width={width < 500 ? "100" : "200"}
+          height={width < 1024 ? "45" : "50"}
           className="relative"
         />
       </Link>

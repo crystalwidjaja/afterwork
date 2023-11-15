143af4864e97a898d0a0e1f897f3e13699a31528
Sophia <sofaz2214@gmail.com>
2023-11-15 14:11:31 -0800

    go back to da old one

---
+++ src/app/page/events.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import Navigation from "../../components/nav";
+import React from "react";
+
+export function Events() {
+  return (
+    <main>
+      <p>events page</p>
+    </main>
+  )
+}

4db54fbf0208e8b68af15746d0b2400f2f93fd10
10084z <127453863+10084z@users.noreply.github.com>
2023-11-27 18:11:57 -0800

    test test
    css on landing page

--- src/app/components/Questions.tsx
+++ src/app/components/Questions.tsx
@@ -1,35 +1,61 @@
+
 import React, { useState } from 'react';
+import './Questions.css'
+
 
 const buttonStyle = "border-2 bg-blue-500 text-white"
 
+
 const majorCities = ["", "Chicago", "Los Angeles", "New York City", "San Francisco"]
 
-const tiers = [{tier: "Standard", image:"money.jpeg", pricing:"up to $1,000", description:"For the cost-concious planner who wants to create a remarkable experience without exceeding a tight budget"},
-                {tier: "Mid-tier", image:"money.jpeg", pricing:"up to $5,000", description:"For the cost-concious planner who wants to create a remarkable experience without exceeding a tight budget"},
-                {tier: "Premium", image:"money.jpeg", pricing:"up to $10,000", description:"For the cost-concious planner who wants to create a remarkable experience without exceeding a tight budget"}
-            ]
 
-const vibes = [{vibe: "Corporate", image: "corporate-office.jpeg"},
-                {vibe: "Gala", image: "money.jpeg"},
-                {vibe: "Social", image: "corporate-office.jpeg"}
+const tiers = [{ tier: "Standard", image: "money.jpeg", pricing: "up to $1,000", description: "For the cost-concious planner who wants to create a remarkable experience without exceeding a tight budget" },
+{ tier: "Mid-tier", image: "money.jpeg", pricing: "up to $5,000", description: "For the cost-concious planner who wants to create a remarkable experience without exceeding a tight budget" },
+{ tier: "Premium", image: "money.jpeg", pricing: "up to $10,000", description: "For the cost-concious planner who wants to create a remarkable experience without exceeding a tight budget" }
 ]
+
+
+const vibes = [{ vibe: "Corporate", image: "corporate-office.jpeg" },
+{ vibe: "Gala", image: "money.jpeg" },
+{ vibe: "Social", image: "corporate-office.jpeg" }
+]
+
 
 const services = ["DJ", "Panel", "Catering"]
 
-export function Landing({ formID, visible, goBack, goNext }){
+
+export function Landing({ formID, visible, goBack, goNext }) {
     return (
-        <div class={visible ? "block":"hidden"}>
-            <p>Welcome to the planner!</p>
-            <p>We'll be using AI to generate all the events in the area according to your choosing</p>
-            <button class={buttonStyle} onClick={goNext}>Click to go next</button>
+        <div className={`container ${visible ? 'block' : 'hidden'}`}>
+            <div className="flex items-center justify-center h-full font-semibold text-3x1">
+                <div className="text-center">
+                    <p className='mb-4'>Welcome to the planner!</p>
+                    <p className='mb-4'>We'll be using AI to generate all the events in the area according to your choosing</p>
+                    <button
+                        className={`${buttonStyle} px-4 py-2 rounded`}
+                        style={{
+                            backgroundImage: 'linear-gradient(to bottom right, #1539CE, #15CECE)',
+                            color: '#ffffff',
+                            borderRadius: '0.375rem',
+                        }}
+                        onClick={goNext}
+                    >
+                        Click to Begin
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
 
-export function WhereHeld({ formID, visible, goBack, goNext }){
+
+
+
+export function WhereHeld({ formID, visible, goBack, goNext }) {
     return (
-        <div class={visible ? "block":"hidden"}>
+        <div class={visible ? "block" : "hidden"}>
             <p>Lets get some basic info about your event</p>
+
 
             <form id={formID}>
                 <label htmlFor="location">Your event will be held in </label>
@@ -38,22 +64,26 @@ export function WhereHeld({ formID, visible, goBack, goNext }){
                 </select>
             </form>
 
+
             <button class={buttonStyle} onClick={goBack}>Previous</button>
             <button class={buttonStyle} onClick={goNext}>Next</button>
         </div>
     )
 }
 
-export function GuestAmount({ formID, visible, goBack, goNext }){
+
+export function GuestAmount({ formID, visible, goBack, goNext }) {
     return (
-        <div class={visible ? "block":"hidden"}>
+        <div class={visible ? "block" : "hidden"}>
             <p>Lets get some basic info about your event</p>
+
 
             <form id={formID}>
                 <label htmlFor="guestAmount">I am expecting an average of </label>
-                <input class="border-2 bg-gray-200" type="number"/>
+                <input class="border-2 bg-gray-200" type="number" />
                 <a>guests</a>
             </form>
+
 
             <button class={buttonStyle} onClick={goBack}>Previous</button>
             <button class={buttonStyle} onClick={goNext}>Next</button>
@@ -61,35 +91,38 @@ export function GuestAmount({ formID, visible, goBack, goNext }){
     )
 }
 
-function BudgetChoice({ checked, onSelect, tier, pricing, image, description}){
+
+function BudgetChoice({ checked, onSelect, tier, pricing, image, description }) {
     return (
         <div class="border">
             <label htmlFor={tier} onClick={() => onSelect(tier)}>
-            <input class="absolute w-0 h-0" type="radio" name="budget"/>
-            <div class={`cursor-pointer ${checked ? "bg-blue-800 text-white":"bg-white text-blue-800"}`}>
-                <p>{tier}</p>
-                <p>{pricing}</p>
-                <img class="w-10 h-10" src={image}/>
-                <p>{description}</p>
-            </div>
+                <input class="absolute w-0 h-0" type="radio" name="budget" />
+                <div class={`cursor-pointer ${checked ? "bg-blue-800 text-white" : "bg-white text-blue-800"}`}>
+                    <p>{tier}</p>
+                    <p>{pricing}</p>
+                    <img class="w-10 h-10" src={image} />
+                    <p>{description}</p>
+                </div>
             </label>
         </div>
     )
 }
 
 
-export function Budget({ formID, visible, goBack, goNext }){
+
+
+export function Budget({ formID, visible, goBack, goNext }) {
     const [selection, changeSelection] = useState("")
     return (
-        <div class={visible ? "block":"hidden"}>
+        <div class={visible ? "block" : "hidden"}>
             <p>What's your budget</p>
             <form id={formID}>
-                {tiers.map((info, index) => <BudgetChoice checked={info.tier==selection}
-                                                    onSelect={changeSelection}
-                                                    tier={info.tier}
-                                                    pricing={info.pricing}
-                                                    image={info.image}
-                                                    description={info.description}/>)}
+                {tiers.map((info, index) => <BudgetChoice checked={info.tier == selection}
+                    onSelect={changeSelection}
+                    tier={info.tier}
+                    pricing={info.pricing}
+                    image={info.image}
+                    description={info.description} />)}
             </form>
             <div>
                 <button class={buttonStyle} onClick={goBack}>Previous</button>
@@ -99,30 +132,32 @@ export function Budget({ formID, visible, goBack, goNext }){
     )
 }
 
-function VibeChoice({ checked, vibe, image, onSelect }){
+
+function VibeChoice({ checked, vibe, image, onSelect }) {
     return (
         <div class="border">
             <label htmlFor={vibe} onClick={() => onSelect(vibe)}>
-            <input class="absolute w-0 h-0" type="radio" name="budget"/>
-            <div class={`cursor-pointer ${checked ? "bg-blue-800 text-white":"bg-white text-blue-800"}`}>
-                <p>{vibe}</p>
-                <img class="" src={image}/>
-            </div>
+                <input class="absolute w-0 h-0" type="radio" name="budget" />
+                <div class={`cursor-pointer ${checked ? "bg-blue-800 text-white" : "bg-white text-blue-800"}`}>
+                    <p>{vibe}</p>
+                    <img class="" src={image} />
+                </div>
             </label>
         </div>
     )
 }
 
-export function Vibe({ formID, visible, goBack, goNext }){
+
+export function Vibe({ formID, visible, goBack, goNext }) {
     const [selection, changeSelection] = useState("")
     return (
-        <div class={visible ? "block":"hidden"}>
+        <div class={visible ? "block" : "hidden"}>
             <p>What's the vibe?</p>
             <form id={formID}>
-                {vibes.map((info, index) => <VibeChoice checked={info.vibe==selection}
-                                                    onSelect={changeSelection}
-                                                    vibe={info.vibe}
-                                                    image={info.image}/>)}
+                {vibes.map((info, index) => <VibeChoice checked={info.vibe == selection}
+                    onSelect={changeSelection}
+                    vibe={info.vibe}
+                    image={info.image} />)}
             </form>
             <div>
                 <button class={buttonStyle} onClick={goBack}>Previous</button>
@@ -132,15 +167,16 @@ export function Vibe({ formID, visible, goBack, goNext }){
     )
 }
 
-export function Services({ formID, visible, goBack, goNext }){
+
+export function Services({ formID, visible, goBack, goNext }) {
     return (
-        <div class={visible ? "block":"hidden"}>
+        <div class={visible ? "block" : "hidden"}>
             <p>What services are you looking for?</p>
             <p>This will help us provide you the best recommendations for your event</p>
             <form id={formID}>
                 {services.map((service) => (<div>
-                <input type="checkbox" id={service} name={service} value={service}/>
-                <label for={service}> {service} </label>
+                    <input type="checkbox" id={service} name={service} value={service} />
+                    <label for={service}> {service} </label>
                 </div>
                 ))}
             </form>
@@ -152,21 +188,24 @@ export function Services({ formID, visible, goBack, goNext }){
     )
 }
 
-export function Submit({ formID, visible, goBack, goNext }){
+
+export function Submit({ formID, visible, goBack, goNext }) {
     return (
-        <div class={visible ? "block":"hidden"}>
-            <input form={formID} class="border-2 text-xl" type="submit" value="Ready to Submit?"/>
-            <br/>
+        <div class={visible ? "block" : "hidden"}>
+            <input form={formID} class="border-2 text-xl" type="submit" value="Ready to Submit?" />
+            <br />
             <button class={buttonStyle} onClick={goBack}>Previous</button>
         </div>
     )
 }
 
-export function Final({ formID, visible, goBack, goNext }){
+
+export function Final({ formID, visible, goBack, goNext }) {
     return (
-        <div class={visible ? "block":"hidden"}>
+        <div class={visible ? "block" : "hidden"}>
             <p>Creating your new itinerary...</p>
         </div>
     )
 }
+
 
--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
       <div className="w-full h-20 bg-white sticky top-0">
         <div className="container mx-auto px-4 h-full">
           <div className="flex justify-between items-center h-full">
-          <Logo />
+            <Logo />
             <ul className="hidden md:flex gap-x-6 text-black">
               <li>
                 <Link href="/about">

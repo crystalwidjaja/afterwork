f1e69362d01dc646c1aa8e38992ee72a62992c11
10084z <127453863+10084z@users.noreply.github.com>
2023-12-02 18:00:06 -0800

    updated questionnaire
    still in progress

--- src/app/components/Questionnaire.tsx
+++ src/app/components/Questionnaire.tsx
@@ -1,42 +1,54 @@
 "use client";
+// Questionnaire.tsx
 import React, { useState } from 'react';
-import {Landing, WhereHeld, GuestAmount, Budget, Vibe, Services, Submit, Final} from './Questions';
+import { Landing, WhereHeld, GuestAmount, Budget, Vibe, Services, Submit, Final } from './Questions';
 
-const questions = [Landing, WhereHeld, GuestAmount, Budget, Vibe, Services, Submit, Final]
+const questions = [Landing, WhereHeld, GuestAmount, Budget, Vibe, Services, Submit, Final];
 
-function ProgressDots({currentQuestion, totalQuestions, onSelect}){
-    return (
-        <div>
-            {[...Array(totalQuestions).keys()].map((index: number) => (
-            <button class={`h-3 w-3 rounded-full ${currentQuestion==index ? 'bg-red-200' : 'bg-blue-200'}`}
-                onClick={() => onSelect(index)}
-            />            
-            ))}
-        </div>
-    )
+interface ProgressDotsProps {
+    currentQuestion: number;
+    totalQuestions: number;
+    onSelect: (index: number) => void;
 }
 
-export default function Questionnaire(){ 
+function ProgressDots({ currentQuestion, totalQuestions, onSelect }: ProgressDotsProps) {
+    return (
+        <div className="flex items-center space-x-2">
+            {[...Array(totalQuestions).keys()].map((index) => (
+                <button
+                    key={index}
+                    className={`h-3 w-3 rounded-full ${currentQuestion === index ? 'bg-red' : 'bg-blue'} border border-black`}
+                    onClick={() => onSelect(index)}
+                />
+            ))}
+        </div>
+    );
+}
+
+export default function Questionnaire() {
     const [currentQuestion, setQuestion] = useState(0);
 
     return (
-        <div class="max-w-screen-md max-h-screen-md mx-auto content-center text-black border-2">
-            <div>
-            <form id="questionnaire" class="hidden" onSubmit={()=>alert("hi")}/>
-            {questions.map((Question, index) => (
-                <Question 
-                    formID="questionnaire"
-                    visible={index === currentQuestion}
-                    goBack = {()=>setQuestion(currentQuestion - 1)}
-                    goNext = {()=>setQuestion(currentQuestion + 1)}
+        <div style={{ backgroundColor: 'rgba(17, 43, 100, 0.58)' }} className="min-h-screen flex items-center justify-center">
+            <div className="max-w-screen-md max-h-screen-md mx-auto content-center text-black border-2 bg-white p-8 rounded-md" style={{ width: '700px', height: '300px' }}>
+                <form id="questionnaire" className="hidden" onSubmit={() => alert('hi')} />
+                {questions.map((Question, index) => (
+                    <Question
+                        key={index}
+                        formID="questionnaire"
+                        visible={index === currentQuestion}
+                        goBack={() => setQuestion(currentQuestion - 1)}
+                        goNext={() => setQuestion(currentQuestion + 1)}
                     />
-                    )
-            )}
-            </div>
-            
-            <div class="flex-end">
-            <ProgressDots currentQuestion = {currentQuestion} totalQuestions = {questions.length-1} onSelect = {setQuestion}/>
+                ))}
+                <div className="mt-8 flex items-center justify-center mt-8 space-x-2">
+                    <ProgressDots
+                        currentQuestion={currentQuestion}
+                        totalQuestions={questions.length - 1}
+                        onSelect={setQuestion}
+                    />
+                </div>
             </div>
         </div>
-    )
-} 
+    );
+}
